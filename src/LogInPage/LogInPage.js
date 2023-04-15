e4d579d112280ab5803e86cb84279a03a19bb287
Chenwukong <77747944+Chenwukong@users.noreply.github.com>
2023-04-15 11:15:23 -0500

    changes

--- src/LogInPage/LogInPage.js
+++ src/LogInPage/LogInPage.js
@@ -19,44 +19,46 @@ const LogInPage = () => {
     setPassword(event.target.value);
   }
 
-  // async function handleLogin(event) {
-  //   event.preventDefault();
+  async function handleLogin(event) {
+    event.preventDefault();
 
-  //   // Make a request to the authentication endpoint
-  //   const response = await fetch("/api/login", {
-  //     method: "POST",
-  //     body: JSON.stringify({ username, password }),
-  //     headers: {
-  //       "Content-Type": "application/json",
-  //     },
-  //   });
+    // Make a request to the authentication endpoint
+    const response = await fetch("https://w5046166g6.goho.co/user", {
+      method: "GET",
+      // body: JSON.stringify({ username, password }),
+      headers: {
+        "Content-Type": "application/json",
+        "token":"eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI3IiwiaWF0IjoxNjgxMDYwNzQ4LCJleHAiOjE2ODE2NjU1NDh9.nP4MFH28zQGmLIl8gJI6odTws4sDo1EdjVzJaNI-bxQSuvmhU31-jv6BPPhURHNisZx_lObnwc7mCz-Fq2-Gfg",
+      },
+    });
 
-  //   if (response.ok) {
-  //     // Login is successful, set isLoggedIn state variable to true
-  //     dispatch(setIsLoggedIn(true));
+    const responseData = await response.json(); // Parse the response as JSON
 
-  //     // Navigate to the main page
-  //     navigate("/main");
-  //   } else {
-  //     // Display an error message
-  //     alert("Invalid username or password");
-  //   }
-  // }
+    console.log(responseData.token); // Log the JSON data
+    console.log(responseData)
+    if (response.ok) {
+      // Login is successful, set isLoggedIn state variable to true
+      dispatch(setIsLoggedIn(true));
+      console.log(response);
+      // Navigate to the main page
+      navigate("/main");
+    } else {
+      // Display an error message
+      alert("Invalid username or password");
+    }
+  }
 
-  const logs = () => {
-    dispatch(setIsLoggedIn(true));
-    navigate("/main");
-  };
-  useEffect(() => {
-    console.log(bgv);
-  });
+  // const logs = () => {
+  //   dispatch(setIsLoggedIn(true));
+  //   navigate("/main");
+  // };
   return (
     <div className="loginPage">
       <video autoPlay muted loop>
         <source src={bgv} type="audio/mp4" />
       </video>
       <div className="login loginBoard">
-        <form onSubmit={logs}>
+        <form>
           <label htmlFor="username">账号:</label>
           <input
             type="text"
@@ -75,7 +77,7 @@ const LogInPage = () => {
           />
           <br />
 
-          <button type="submit" className="button-85">
+          <button type="submit" className="button-85" onClick={handleLogin}>
             登录
           </button>
         </form>
